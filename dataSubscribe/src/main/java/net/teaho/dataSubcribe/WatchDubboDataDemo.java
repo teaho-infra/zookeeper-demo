da65d7e6bc7d4ebb2cb837a2ac0da81e8ac21db4
hetingliang <hetl@akulaku.com>
2019-06-16 22:00:38 +0800

    [upd]

--- dataSubscribe/src/main/java/net/teaho/dataSubcribe/WatchDubboDataDemo.java
+++ dataSubscribe/src/main/java/net/teaho/dataSubcribe/WatchDubboDataDemo.java
@@ -2,23 +2,33 @@ package net.teaho.dataSubcribe;
 
 
 import com.google.common.base.Charsets;
+import org.I0Itec.zkclient.IZkChildListener;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.exception.ZkMarshallingError;
-import org.I0Itec.zkclient.serialize.SerializableSerializer;
 import org.I0Itec.zkclient.serialize.ZkSerializer;
 import org.apache.zookeeper.data.Stat;
 
-public class GetCommand {
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
-    public static void main(String[] args) {
-        ZkClient zc = new ZkClient("39.108.53.140:8181",10000,10000,new MyZkSerializer());
+public class WatchDubboDataDemo {
+
+    private static final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    private static final String VIDEO_DUBBO_PATH = "/dubbo/com.kalikali.videoservice.sdk.api.VideoServiceApi/providers";
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ZkClient zc = new ZkClient("xxx",10000,10000,new MyZkSerializer());
+        zc.subscribeChildChanges(VIDEO_DUBBO_PATH, childListener);
         System.out.println("conneted ok!");
-        
+
         Stat stat = new Stat();
-        String u = zc.readData("/dubbo/com.kalikali.videoservice.sdk.api.VideoServiceApi/providers",stat);
+        String u = zc.readData(VIDEO_DUBBO_PATH, stat);
         System.out.println(u);
         System.out.println(stat);
-        
+
+        countDownLatch.await();
     }
 
 
@@ -37,6 +47,13 @@ public class GetCommand {
             return new String(bytes, Charsets.UTF_8);
         }
     }
+
+    public static final IZkChildListener childListener = (parentPath, currentChilds) -> {
+        System.out.println("work server list changed, new list is ");
+        System.out.println(currentChilds);
+
+
+    };
 
 
 }
